b4f8c7bcf50818975d37251b6ccce62f7c9a4a0b
Shaher-11 <shahershamroukh@gmail.com>
2021-04-29 10:35:25 +0200

    Add devise gem and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :secrets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "secrets#index"
